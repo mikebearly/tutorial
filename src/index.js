d1237bd01f07fc7e96fc01c68fb056189c0f0be9
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-06-26 21:12:11 +0530

    Mini books with 2 books

--- src/index.js
+++ src/index.js
@@ -4,24 +4,44 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // stateles funcational component
 // always return JSX
-const author = "Navis Michael Bearly J";
-const title = "Mathematics Textbook For Class 6 Language English";
-const img = "https://m.media-amazon.com/images/I/41Z7tGl7MGL._AC_UY218_.jpg";
+
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/41Z7tGl7MGL._AC_UY218_.jpg",
+  title: "Mathematics Textbook For Class 6 Language English",
+  author: "Navis Michael Bearly J",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/81hyid7dI5L._AC_UY218_.jpg",
+  title: "1001 Activities Book",
+  author: "Dreamland Publications",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
     </article>
   );
 };
