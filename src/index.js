0285a4456660a19a80448ad8e0b5b94e0adba71d
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-06-26 21:43:07 +0530

    props list in normal way

--- src/index.js
+++ src/index.js
@@ -5,44 +5,37 @@ import "./index.css";
 // stateles funcational component
 // always return JSX
 
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/41Z7tGl7MGL._AC_UY218_.jpg",
-  title: "Mathematics Textbook For Class 6 Language English",
-  author: "Navis Michael Bearly J",
-};
-
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/81hyid7dI5L._AC_UY218_.jpg",
-  title: "1001 Activities Book",
-  author: "Dreamland Publications",
-};
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/41Z7tGl7MGL._AC_UY218_.jpg",
+    title: "Mathematics Textbook For Class 6 Language English",
+    author: "Navis Michael Bearly J",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/81hyid7dI5L._AC_UY218_.jpg",
+    title: "1001 Activities Book",
+    author: "Dreamland Publications",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>lorem ipsum</p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
