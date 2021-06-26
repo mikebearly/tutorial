67cb1f4789037465258f1f4189a401529bb8630b
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-06-26 21:55:03 +0530

    use props with spread operator

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     img: "https://m.media-amazon.com/images/I/41Z7tGl7MGL._AC_UY218_.jpg",
     title: "Mathematics Textbook For Class 6 Language English",
     author: "Navis Michael Bearly J",
   },
   {
+    id: 2,
     img: "https://m.media-amazon.com/images/I/81hyid7dI5L._AC_UY218_.jpg",
     title: "1001 Activities Book",
+    author: "Dreamland Publications",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/81JkY8MrorL._AC_UY218_.jpg",
+    title: "Copy colour - Fruit",
     author: "Dreamland Publications",
   },
 ];
@@ -22,15 +30,14 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props.book;
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
