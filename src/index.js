0f7679cde1f6ed2899e2069398e13f62a1905e78
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-06-26 21:16:03 +0530

    Mini Book - props destructing directly

--- src/index.js
+++ src/index.js
@@ -34,8 +34,7 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
